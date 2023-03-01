bae98aa3e8f84a29ebc3bb1d037c94a4c21249a2
kaktuz <kaktuz911@gmail.com>
2023-03-01 17:33:13 +0200

    update all

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,13 @@
-// // Отримуємо всі елементи <header> на сторінці
-// const headers = document.getElementsByTagName("header");
+// Отримуємо всі елементи <header> на сторінці
+const headers = document.getElementsByTagName("header");
 
-// // Отримуємо назву поточної сторінки з URL
-// const currentPageName = window.location.pathname.split("/").pop().split(".")[0];
+// Отримуємо назву поточної сторінки з URL
+const currentPageName = window.location.pathname.split("/").pop().split(".")[0];
 
-// // Перевіряємо, чи поточна сторінка є сторінкою "hotel", "restaurant" або "room"
-// if (["hotel", "restaurant", "room"].includes(currentPageName)) {
-//   // Проходимось по всіх елементах <header> і додаємо клас "new-class"
-//   for (let i = 0; i < headers.length; i++) {
-//     headers[i].classList.add("revers");
-//   }
-// }
+// Перевіряємо, чи поточна сторінка є сторінкою "hotel", "restaurant" або "room"
+if (["hotel", "restaurant", "room"].includes(currentPageName)) {
+  // Проходимось по всіх елементах <header> і додаємо клас "new-class"
+  for (let i = 0; i < headers.length; i++) {
+    headers[i].classList.add("revers");
+  }
+}
